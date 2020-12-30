8f5493c075bfcb2347445799a4eae4dc5b89b19e
Laskel96 <tls09611@naver.com>
2020-12-30 19:16:15 +0900

    Remove unnecessary Components  - Run Reformat code, Optimize Import

--- app/src/main/java/com/example/diet/ui/UserProfileInputActivity.kt
+++ app/src/main/java/com/example/diet/ui/UserProfileInputActivity.kt
@@ -7,15 +7,19 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.diet.R
 import kotlinx.android.synthetic.main.activity_user_profile_input.*
 
-class UserProfileInputActivity : AppCompatActivity(){
+class UserProfileInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile_input)
         completeButton.setOnClickListener {
-            val userName : String = nameEditView.getText().toString()
-            val userHeight : Int = heightEditView.getText().toString().toInt()
-            val userAge : Int = ageEditView.getText().toString().toInt()
-            Toast.makeText(applicationContext, "$userName : $userHeight : $userAge", Toast.LENGTH_LONG).show()
+            val userName: String = nameEditView.getText().toString()
+            val userHeight: Int = heightEditView.getText().toString().toInt()
+            val userAge: Int = ageEditView.getText().toString().toInt()
+            Toast.makeText(
+                applicationContext,
+                "$userName : $userHeight : $userAge",
+                Toast.LENGTH_LONG
+            ).show()
             startActivity(Intent(this, UserWeightInputActivity::class.java))
         }
     }
--- app/src/main/java/com/example/diet/ui/UserWeightInputActivity.kt
+++ app/src/main/java/com/example/diet/ui/UserWeightInputActivity.kt
@@ -1,20 +1,18 @@
 package com.example.diet.ui
+
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.diet.R
 import kotlinx.android.synthetic.main.activity_user_weight_input.*
-import java.text.DateFormat.getDateInstance
 import java.text.SimpleDateFormat
 import java.util.*
 
-class UserWeightInputActivity : AppCompatActivity(){
+class UserWeightInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_weight_input)
         val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         val currentDate = dateFormat.format(Date())
         dateTextView.setText(currentDate.toString())
-        weightInfoButton.isEnabled = false
     }
 }
