80b94026a268332a13f7fd5668c2c9b09e23a1b5
Laskel96 <tls09611@naver.com>
2020-12-30 20:21:45 +0900

    Add Complete Button

--- app/src/main/java/com/example/diet/ui/UserWeightInputActivity.kt
+++ app/src/main/java/com/example/diet/ui/UserWeightInputActivity.kt
@@ -1,6 +1,7 @@
 package com.example.diet.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.diet.R
 import kotlinx.android.synthetic.main.activity_user_weight_input.*
@@ -14,5 +15,15 @@ class UserWeightInputActivity : AppCompatActivity() {
         val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         val currentDate = dateFormat.format(Date())
         dateTextView.setText(currentDate.toString())
+        completeButton.setOnClickListener {
+            val weight: Int = weightEditText.getText().toString().toInt()
+            val percentageBodyFat: Int = percentageBodyFatEditText.getText().toString().toInt()
+            val amountOfMuscle: Int = amountOfMuscleEditText.getText().toString().toInt()
+            Toast.makeText(
+                applicationContext,
+                "$weight : $percentageBodyFat : $amountOfMuscle",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
