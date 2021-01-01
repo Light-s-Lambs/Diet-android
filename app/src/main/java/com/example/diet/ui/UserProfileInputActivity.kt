42693a3590c76ab810f092196073f7c443678065
Laskel96 <31683011+Laskel96@users.noreply.github.com>
2021-01-01 14:06:14 +0900

    Add Weight Input Activity (#2)
    * Add Weight Input Activity
    
    * Remove unnecessary Components
     - Run Reformat code, Optimize Import
    
    * Add Complete Button
    
    * Remove unnecessary Components
     - remove Temporary Complte Button
    
    (cherry picked from commit f76d7500315b083058881ff1d150a90b4ee619eb)

--- app/src/main/java/com/example/diet/ui/UserProfileInputActivity.kt
+++ app/src/main/java/com/example/diet/ui/UserProfileInputActivity.kt
@@ -1,20 +1,26 @@
 package com.example.diet.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.diet.R
 import kotlinx.android.synthetic.main.activity_user_profile_input.*
 
-class UserProfileInputActivity : AppCompatActivity(){
+class UserProfileInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile_input)
         completeButton.setOnClickListener {
-            val userName : String = nameEditView.getText().toString()
-            val userHeight : Int = heightEditView.getText().toString().toInt()
-            val userAge : Int = ageEditView.getText().toString().toInt()
-            Toast.makeText(applicationContext, "$userName : $userHeight : $userAge", Toast.LENGTH_LONG).show()
+            val userName: String = nameEditView.getText().toString()
+            val userHeight: Int = heightEditView.getText().toString().toInt()
+            val userAge: Int = ageEditView.getText().toString().toInt()
+            Toast.makeText(
+                applicationContext,
+                "$userName : $userHeight : $userAge",
+                Toast.LENGTH_LONG
+            ).show()
+            startActivity(Intent(this, UserWeightInputActivity::class.java))
         }
     }
 }
