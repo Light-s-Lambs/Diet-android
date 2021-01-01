5f3fcddd6947c26e89757982386f2124a98833b3
krbgh205760 <krbgh205760@naver.com>
2021-01-01 22:27:46 +0900

    UserLifeStyleInputActivity.kt 추가

---
+++ app/src/main/java/com/example/diet/ui/UserLifeStyleInputActivity.kt
@@ -0,0 +1,21 @@
+package com.example.diet.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.diet.R
+import kotlinx.android.synthetic.main.activity_user_life_style_input.*
+
+
+class UserLifeStyleInputActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_life_style_input)
+        exitButton.setOnClickListener {
+            finish()
+        }
+        addButton.setOnClickListener {
+        }
+        saveButton.setOnClickListener {
+        }
+    }
+}
