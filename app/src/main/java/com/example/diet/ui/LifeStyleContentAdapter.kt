c5a78ce3691d16f153fc72ce188c9a9e74aa864b
Laskel96 <tls09611@naver.com>
2021-01-05 22:06:13 +0900

    Objectifying LifeStyleDiffCallback

--- app/src/main/java/com/example/diet/ui/LifeStyleContentAdapter.kt
+++ app/src/main/java/com/example/diet/ui/LifeStyleContentAdapter.kt
@@ -3,6 +3,7 @@ package com.example.diet.ui
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.diet.R
 
@@ -10,7 +11,21 @@ class LifeStyleContentAdapter(
     private val context: Context,
     private val lifeStyleList: List<LifeStyle>
 ) : androidx.recyclerview.widget.ListAdapter<LifeStyle, RecyclerView.ViewHolder>(
-    LifeStyleDiffCallback()
+    object : DiffUtil.ItemCallback<LifeStyle>(){
+        override fun areItemsTheSame(
+            oldItem: LifeStyle,
+            newItem: LifeStyle
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(
+            oldItem: LifeStyle,
+            newItem: LifeStyle
+        ): Boolean {
+            return oldItem.name == newItem.name && oldItem.burnedCalorie == oldItem.burnedCalorie && oldItem.time == oldItem.time
+        }
+    }
 ) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view =
--- app/src/main/java/com/example/diet/ui/LifeStyleSectionHeaderAdapter.kt
+++ app/src/main/java/com/example/diet/ui/LifeStyleSectionHeaderAdapter.kt
@@ -3,13 +3,28 @@ package com.example.diet.ui
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.diet.R
 
 class LifeStyleSectionHeaderAdapter(
     private val context: Context
 ) : androidx.recyclerview.widget.ListAdapter<LifeStyle, RecyclerView.ViewHolder>(
-    LifeStyleDiffCallback()
+    object : DiffUtil.ItemCallback<LifeStyle>(){
+        override fun areItemsTheSame(
+            oldItem: LifeStyle,
+            newItem: LifeStyle
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(
+            oldItem: LifeStyle,
+            newItem: LifeStyle
+        ): Boolean {
+            return oldItem.name == newItem.name && oldItem.burnedCalorie == oldItem.burnedCalorie && oldItem.time == oldItem.time
+        }
+    }
 ) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view =
