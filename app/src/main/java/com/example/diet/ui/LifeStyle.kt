a37394c7fa6396efc5c5c5f49088e9f2e8815fd9
Laskel96 <tls09611@naver.com>
2021-01-06 21:51:01 +0900

    Change LifeStyle class to data class

--- app/src/main/java/com/example/diet/ui/LifeStyle.kt
+++ app/src/main/java/com/example/diet/ui/LifeStyle.kt
@@ -1,3 +1,3 @@
 package com.example.diet.ui
 
-class LifeStyle(val name: String, val time: String, val burnedCalorie: String)
+data class LifeStyle(val name: String, val time: String, val burnedCalorie: String)
