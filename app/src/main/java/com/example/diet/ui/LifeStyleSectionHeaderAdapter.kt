96d4fe0d36c768d7228617407d7047cf1ce3ff20
Laskel96 <tls09611@naver.com>
2021-01-13 00:53:18 +0900

    Change list adapter to rcyclerview

--- app/src/main/java/com/example/diet/ui/LifeStyleSectionHeaderAdapter.kt
+++ app/src/main/java/com/example/diet/ui/LifeStyleSectionHeaderAdapter.kt
@@ -3,27 +3,12 @@ package com.example.diet.ui
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.diet.R
 
 class LifeStyleSectionHeaderAdapter(
     private val context: Context
-) : ListAdapter<LifeStyle, RecyclerView.ViewHolder>(
-    object : DiffUtil.ItemCallback<LifeStyle>() {
-        override fun areItemsTheSame(
-            oldItem: LifeStyle,
-            newItem: LifeStyle
-        ): Boolean = areContentsTheSame(oldItem, newItem)
-
-        override fun areContentsTheSame(
-            oldItem: LifeStyle,
-            newItem: LifeStyle
-        ): Boolean =
-            oldItem == newItem
-    }
-) {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val listSize = 1
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view =
