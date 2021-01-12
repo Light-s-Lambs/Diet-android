f2843f946089f8a5725bd95afdeea2a0df38475c
Laskel96 <tls09611@naver.com>
2021-01-13 00:40:54 +0900

    Change Unnecessary list adapter to recyclerview

--- app/src/main/java/com/example/diet/ui/MealSectionHeaderAdapter.kt
+++ app/src/main/java/com/example/diet/ui/MealSectionHeaderAdapter.kt
@@ -3,27 +3,12 @@ package com.example.diet.ui
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.diet.R
 
 class MealSectionHeaderAdapter(
     private val context: Context
-) : ListAdapter<Meal, RecyclerView.ViewHolder>(
-    object : DiffUtil.ItemCallback<Meal>() {
-        override fun areItemsTheSame(
-            oldItem: Meal,
-            newItem: Meal
-        ): Boolean = areContentsTheSame(oldItem, newItem)
-
-        override fun areContentsTheSame(
-            oldItem: Meal,
-            newItem: Meal
-        ): Boolean =
-            oldItem == newItem
-    }
-) {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view =
             LayoutInflater.from(context).inflate(R.layout.user_meal_list_item, parent, false)
