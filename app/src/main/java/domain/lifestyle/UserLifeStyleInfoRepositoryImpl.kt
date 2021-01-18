7beeac02c9995398b89b2d56c51f1ce698b410a7
Laskel96 <tls09611@naver.com>
2021-01-18 21:59:34 +0900

    Add TODO()

--- app/src/main/java/domain/lifestyle/UserLifeStyleInfoRepositoryImpl.kt
+++ app/src/main/java/domain/lifestyle/UserLifeStyleInfoRepositoryImpl.kt
@@ -2,12 +2,10 @@ package domain.lifestyle
 
 class UserLifeStyleInfoRepositoryImpl : UserLifeStyleInfoRepository {
     override fun save(date: String, activityMetabolism: String): Boolean {
-        // TODO: 2021-01-15
-        return true
+        TODO()
     }
 
     override fun delete(date: String): Boolean {
-        // TODO: 2021-01-15
-        return true
+        TODO()
     }
 }
