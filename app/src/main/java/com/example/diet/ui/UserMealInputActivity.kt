f817951d7c1d823bb07887f72439441abf688e73
krbgh205760 <krbgh205760@naver.com>
2021-01-19 23:16:20 +0900

    Add User MealInput Activity (#10)
    Meal Input 관련 Activity 추가 및 xml 내용 변경

---
+++ app/src/main/java/com/example/diet/ui/UserMealInputActivity.kt
@@ -0,0 +1,13 @@
+package com.example.diet.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.diet.R
+
+
+class UserMealInputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_meal_input)
+    }
+}
