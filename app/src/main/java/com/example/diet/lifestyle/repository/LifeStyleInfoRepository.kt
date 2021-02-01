003697fecab504f4908e20d5d70cc48317b0f616
Laskel96 <tls09611@naver.com>
2021-02-02 00:02:47 +0900

    Delete UpdateLifeStyle Method. Save with Overwrite can do this behavior.

--- app/src/main/java/com/example/diet/lifestyle/repository/LifeStyleInfoRepository.kt
+++ app/src/main/java/com/example/diet/lifestyle/repository/LifeStyleInfoRepository.kt
@@ -5,6 +5,5 @@ import com.example.diet.lifestyle.model.LifeStyleInfo
 interface LifeStyleInfoRepository {
     fun save(date: String, lifeStyleInfo: LifeStyleInfo): Boolean
     fun load(date: String): LifeStyleInfo
-    fun update(date: String, lifeStyleInfo: LifeStyleInfo): Boolean
     fun delete(date: String): Boolean
 }
