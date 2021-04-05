f19e4be078fa8e3d0d563a17de79ebc581bd2cb3
K <krbgh205760@naver.com>
2021-04-06 02:40:52 +0900

    Custom Exception class 추가

---
+++ app/src/main/java/com/example/diet/meal/usecase/exception/DataNoExistException.kt
@@ -0,0 +1,9 @@
+package com.example.diet.meal.usecase.exception
+
+class DataNoExistException : Exception {
+    constructor() : super()
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+}
+
