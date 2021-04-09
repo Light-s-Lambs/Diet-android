a320c7626afbbdf4d93f15d17cd7ecccde34cbf6
K <krbgh205760@naver.com>
2021-04-10 00:15:41 +0900

    오탈자 수정

--- app/src/main/java/com/example/diet/meal/usecase/exception/DataAlreadyExistException.kt
+++ app/src/main/java/com/example/diet/meal/usecase/exception/DataAlreadyExistException.kt
@@ -1,6 +1,6 @@
 package com.example.diet.meal.usecase.exception
 
-class DataAlreadyExIstException : Exception {
+class DataAlreadyExistException : Exception {
     constructor() : super()
     constructor(message: String) : super(message)
     constructor(message: String, cause: Throwable) : super(message, cause)
