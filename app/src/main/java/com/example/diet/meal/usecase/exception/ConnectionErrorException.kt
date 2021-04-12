4eaf29117fef21e7384e7ede5c32c6fede895f1a
krbgh205760 <krbgh205760@naver.com>
2021-04-13 06:52:05 +0900

    JvmOverloads 적용

--- app/src/main/java/com/example/diet/meal/usecase/exception/ConnectionErrorException.kt
+++ app/src/main/java/com/example/diet/meal/usecase/exception/ConnectionErrorException.kt
@@ -1,8 +1,6 @@
 package com.example.diet.meal.usecase.exception
 
-class ConnectionErrorException : Exception {
-    constructor() : super()
-    constructor(message: String) : super(message)
-    constructor(message: String, cause: Throwable) : super(message, cause)
-    constructor(cause: Throwable) : super(cause)
-}
+class ConnectionErrorException @JvmOverloads constructor(
+    message: String? = null,
+    cause: Throwable? = null
+) : Exception(message, cause)
--- app/src/main/java/com/example/diet/meal/usecase/exception/DataAlreadyExistException.kt
+++ app/src/main/java/com/example/diet/meal/usecase/exception/DataAlreadyExistException.kt
@@ -1,9 +1,6 @@
 package com.example.diet.meal.usecase.exception
 
-class DataAlreadyExistException : Exception {
-    constructor() : super()
-    constructor(message: String) : super(message)
-    constructor(message: String, cause: Throwable) : super(message, cause)
-    constructor(cause: Throwable) : super(cause)
-}
-
+class DataAlreadyExistException @JvmOverloads constructor(
+    message: String? = null,
+    cause: Throwable? = null
+) : Exception(message, cause)
