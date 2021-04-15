bbb1af4ecbdf347d86fdba75553bb09d24e6c6b6
krbgh205760 <krbgh205760@naver.com>
2021-04-16 01:35:55 +0900

    연결 실패 예외처리 이름 변경 (ConnectionFailureException) 및 jvmOverloads 어노테이션 제거

--- app/src/main/java/com/example/diet/meal/usecase/exception/ConnectErrorException.kt
+++
@@ -1,6 +0,0 @@
-package com.example.diet.meal.usecase.exception
-
-class ConnectErrorException @JvmOverloads constructor(
-    message: String? = null,
-    cause: Throwable? = null
-) : Exception(message, cause)
