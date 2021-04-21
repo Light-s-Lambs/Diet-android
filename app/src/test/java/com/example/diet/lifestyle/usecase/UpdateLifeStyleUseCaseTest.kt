6a0e2ade85d1d044f05fe4f8d73ad6da886b62ae
Laskel96 <tls09611@naver.com>
2021-04-21 23:40:13 +0900

    UpdateLifeStyleUseCaseTest Method Rename & divide network cases

--- app/src/test/java/com/example/diet/lifestyle/usecase/UpdateLifeStyleUseCaseTest.kt
+++ app/src/test/java/com/example/diet/lifestyle/usecase/UpdateLifeStyleUseCaseTest.kt
@@ -9,10 +9,7 @@ import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 import org.joda.time.DateTime
 import org.junit.Assert.assertEquals
@@ -34,7 +31,7 @@ class UpdateLifeStyleUseCaseTest {
     }
 
     @Test
-    fun `기존 활동에 변경 사항이 있음_활동 갱신 성공`() {
+    fun `기존에 저장된 정보로 만든 LifeStyleRequest 내용이 사용자가 입력한 정보로 만든 LifeStyleRequest 내용과 동일하지 않음_활동 갱신 성공`() {
         val date = DateTime.now()
         val lifeStyleOriginRequest = LifeStyleRequest(date, "Running", 2.0, 1510.0)
         val lifeStyleRevisionRequest = LifeStyleRequest(date, "Sleeping", 22.0, 348.0)
@@ -65,7 +62,7 @@ class UpdateLifeStyleUseCaseTest {
     }
 
     @Test
-    fun `기존 활동에 변경 사항이 없음_활동 갱신 실패`() {
+    fun `기존에 저장된 정보로 만든 LifeStyleRequest 내용이 사용자가 입력한 정보로 만든 LifeStyleRequest 내용과 동일함_활동 갱신 실패`() {
         val date = DateTime.now()
         val lifeStyleOriginRequest = LifeStyleRequest(date, "Running", 2.0, 1510.0)
         val lifeStyleRevisionRequest = LifeStyleRequest(date, "Sleeping", 22.0, 348.0)
@@ -92,7 +89,7 @@ class UpdateLifeStyleUseCaseTest {
     }
 
     @Test
-    fun `기존 활동이 없음_활동 갱신 실패`() {
+    fun `기존에 저장된 정보로 만든 LifeStyleRequest 내용이 존재하지 않음_활동 갱신 실패`() {
         val date = DateTime.now()
         val lifeStyleOriginRequest = LifeStyleRequest(date, "Running", 2.0, 1510.0)
         val lifeStyleRevisionRequest = LifeStyleRequest(date, "Sleeping", 22.0, 348.0)
@@ -119,24 +116,122 @@ class UpdateLifeStyleUseCaseTest {
     }
 
     @Test
-    fun `네트워크 문제_활동 갱신 실패`() {
+    fun `기존에 저장된 정보로 만든 LifeStyleRequest와 사용자가 입력한 정보로 만든 LifeStyleRequest를 보낼때, 네트워크 문제로_3번 재시도_1회차 연결 성공_갱신 성공`() {
         val date = DateTime.now()
         val lifeStyleOriginRequest = LifeStyleRequest(date, "Running", 2.0, 1510.0)
         val lifeStyleRevisionRequest = LifeStyleRequest(date, "Sleeping", 22.0, 348.0)
-        val expected = NetworkFailureException()
+        val lifeStyleRevision = LifeStyle(
+            lifeStyleRevisionRequest.date,
+            lifeStyleRevisionRequest.name,
+            lifeStyleRevisionRequest.time,
+            lifeStyleRevisionRequest.burnedCalorie
+        )
+        var retryUntilZero = 1
+        val maxRetries: Long = 3
+        val exception = NetworkFailureException()
+        val expected = lifeStyleRevision
         coEvery {
             repository.updateLifeStyle(
                 lifeStyleOriginRequest,
                 lifeStyleRevisionRequest
             )
         } returns callbackFlow {
-            close(expected)
+            if (retryUntilZero == 0) {
+                offer(expected)
+                close()
+            } else {
+                retryUntilZero -= 1
+                close(exception)
+            }
         }
 
         runBlocking {
             updateUseCase(
                 lifeStyleOriginRequest,
                 lifeStyleRevisionRequest
+            ).retry(
+                maxRetries
+            ).catch {
+                fail()
+            }.collect {
+                assertEquals(expected, it)
+            }
+        }
+    }
+
+    @Test
+    fun `기존에 저장된 정보로 만든 LifeStyleRequest와 사용자가 입력한 정보로 만든 LifeStyleRequest를 보낼때, 네트워크 문제로_3번 재시도_3회차 연결 성공_갱신 성공`() {
+        val date = DateTime.now()
+        val lifeStyleOriginRequest = LifeStyleRequest(date, "Running", 2.0, 1510.0)
+        val lifeStyleRevisionRequest = LifeStyleRequest(date, "Sleeping", 22.0, 348.0)
+        val lifeStyleRevision = LifeStyle(
+            lifeStyleRevisionRequest.date,
+            lifeStyleRevisionRequest.name,
+            lifeStyleRevisionRequest.time,
+            lifeStyleRevisionRequest.burnedCalorie
+        )
+        var retryUntilZero = 3
+        val maxRetries: Long = 3
+        val exception = NetworkFailureException()
+        val expected = lifeStyleRevision
+        coEvery {
+            repository.updateLifeStyle(
+                lifeStyleOriginRequest,
+                lifeStyleRevisionRequest
+            )
+        } returns callbackFlow {
+            if (retryUntilZero == 0) {
+                offer(expected)
+                close()
+            } else {
+                retryUntilZero -= 1
+                close(exception)
+            }
+        }
+
+        runBlocking {
+            updateUseCase(
+                lifeStyleOriginRequest,
+                lifeStyleRevisionRequest
+            ).retry(
+                maxRetries
+            ).catch {
+                fail()
+            }.collect {
+                assertEquals(expected, it)
+            }
+        }
+    }
+
+    @Test
+    fun `기존에 저장된 정보로 만든 LifeStyleRequest와 사용자가 입력한 정보로 만든 LifeStyleRequest를 보낼때, 네트워크 문제로 3번 재시도_모든 재시도 연결 실패_갱신 실패`() {
+        val date = DateTime.now()
+        val lifeStyleOriginRequest = LifeStyleRequest(date, "Running", 2.0, 1510.0)
+        val lifeStyleRevisionRequest = LifeStyleRequest(date, "Sleeping", 22.0, 348.0)
+        var retryUntilZero = 4
+        val maxRetries: Long = 3
+        val exception = NetworkFailureException()
+        val expected = exception
+        coEvery {
+            repository.updateLifeStyle(
+                lifeStyleOriginRequest,
+                lifeStyleRevisionRequest
+            )
+        } returns callbackFlow {
+            if (retryUntilZero == 0) {
+                close(exception)
+            } else {
+                retryUntilZero -= 1
+                close(exception)
+            }
+        }
+
+        runBlocking {
+            updateUseCase(
+                lifeStyleOriginRequest,
+                lifeStyleRevisionRequest
+            ).retry(
+                maxRetries
             ).catch {
                 assertEquals(expected::class, it::class)
             }.collect {
