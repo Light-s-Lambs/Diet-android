4ff9b1338197fdd542733065e828433d890e12fd
Laskel96 <tls09611@naver.com>
2021-04-24 01:03:47 +0900

    Flatten Method

--- app/src/main/java/com/example/diet/extension/FlowExtension.kt
+++ app/src/main/java/com/example/diet/extension/FlowExtension.kt
@@ -1,18 +1,10 @@
 package com.example.diet.extension
 
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.withTimeout
 
 @FlowPreview
-suspend fun <T> Flow<T>.timeout(timeMillis: Long): Flow<T> {
-    val mainStream = this
-    return flow<T> {
-        withTimeout(timeMillis) {
-            mainStream.debounce(timeMillis).first()
-        }
-    }
+suspend fun <T> Flow<T>.timeout(timeMillis: Long): Flow<T> = flow {
+    withTimeout(timeMillis) { collect { value -> emit(value) } }
 }
