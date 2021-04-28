7a3085b86a01eb5c440e29bc00517747b2b3dd07
Laskel96 <tls09611@naver.com>
2021-04-28 22:23:08 +0900

    Add Gender enum class

--- app/src/main/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismUseCase.kt
+++ app/src/main/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismUseCase.kt
@@ -1,18 +1,19 @@
 package com.example.diet.lifestyle.usecase
 
+import com.example.diet.lifestyle.model.Gender
 import kotlin.math.roundToInt
 
 class CalculateBasalMetabolismUseCase {
-    operator fun invoke(weight: Double, height: Double, age: Int, male: Boolean): Double {
+    operator fun invoke(weight: Double, height: Double, age: Int, gender: Gender): Double {
         require(weight > 0 && height > 0 && age > 0) {
             "Weight, Height, Age Must Be non-negative"
         }
-        return when (male) {
-            true -> {
+        return when (gender) {
+            Gender.male -> {
                 val basalMetabolism: Double = 66.47 + (13.75 * weight) + (5 * height) - (6.76 * age)
                 (basalMetabolism * 10).roundToInt() / 10.0
             }
-            false -> {
+            Gender.female -> {
                 val basalMetabolism: Double =
                     655.1 + (9.56 * weight) + (1.85 * height) - (4.68 * age)
                 (basalMetabolism * 10).roundToInt() / 10.0
