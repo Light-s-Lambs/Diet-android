a3776ed4d1a4ecf30c902c12ab3a8b63a05ac5f4
Laskel96 <tls09611@naver.com>
2021-04-28 23:00:16 +0900

    Rename round method

--- app/src/main/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismUseCase.kt
+++ app/src/main/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismUseCase.kt
@@ -4,10 +4,6 @@ import com.example.diet.lifestyle.model.Gender
 import kotlin.math.roundToInt
 
 class CalculateBasalMetabolismUseCase {
-    private fun roundToFirstDigit(double: Double) : Double{
-        return (double * 10.0).roundToInt() / 10.0
-    }
-
     operator fun invoke(weight: Double, height: Double, age: Int, gender: Gender): Double {
         require(weight > 0 && height > 0 && age > 0) {
             "Weight, Height, Age Must Be non-negative"
@@ -15,13 +11,17 @@ class CalculateBasalMetabolismUseCase {
         return when (gender) {
             Gender.male -> {
                 val basalMetabolism: Double = 66.47 + (13.75 * weight) + (5 * height) - (6.76 * age)
-                roundToFirstDigit(basalMetabolism)
+                roundHundredthsPlace(basalMetabolism)
             }
             Gender.female -> {
                 val basalMetabolism: Double =
                     655.1 + (9.56 * weight) + (1.85 * height) - (4.68 * age)
-                roundToFirstDigit(basalMetabolism)
+                roundHundredthsPlace(basalMetabolism)
             }
         }
+    }
+
+    private fun roundHundredthsPlace(double: Double) : Double{
+        return (double * 10.0).roundToInt() / 10.0
     }
 }
