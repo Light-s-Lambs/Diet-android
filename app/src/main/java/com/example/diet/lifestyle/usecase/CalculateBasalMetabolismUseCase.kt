4d62c0dd34891729d1d9b9d6ba4b9915e4669c1b
Laskel96 <tls09611@naver.com>
2021-04-28 23:13:11 +0900

    Apply Flow

--- app/src/main/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismUseCase.kt
+++ app/src/main/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismUseCase.kt
@@ -1,27 +1,44 @@
 package com.example.diet.lifestyle.usecase
 
 import com.example.diet.lifestyle.model.Gender
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kotlin.math.roundToInt
 
 class CalculateBasalMetabolismUseCase {
-    operator fun invoke(weight: Double, height: Double, age: Int, gender: Gender): Double {
-        require(weight > 0 && height > 0 && age > 0) {
-            "Weight, Height, Age Must Be non-negative"
-        }
-        return when (gender) {
-            Gender.Male -> {
-                val basalMetabolism: Double = 66.47 + (13.75 * weight) + (5 * height) - (6.76 * age)
-                roundHundredthsPlace(basalMetabolism)
+    operator fun invoke(weight: Double, height: Double, age: Int, gender: Gender): Flow<Double> =
+        flow {
+            require(weight > 0 && height > 0 && age > 0) {
+                "Weight, Height, Age Must Be non-negative"
             }
-            Gender.Female -> {
-                val basalMetabolism: Double =
-                    655.1 + (9.56 * weight) + (1.85 * height) - (4.68 * age)
-                roundHundredthsPlace(basalMetabolism)
-            }
-        }
-    }
+            when (gender) {
+                Gender.Male -> {
+                    val basalMetabolism: Double =
+                        66.47 + (13.75 * weight) + (5 * height) - (6.76 * age)
 
-    private fun roundHundredthsPlace(double: Double) : Double{
-        return (double * 10.0).roundToInt() / 10.0
+                    roundHundredthsPlace(
+                        basalMetabolism
+                    ).collect {
+                        emit(it)
+                    }
+                }
+                Gender.Female -> {
+                    val basalMetabolism: Double =
+                        655.1 + (9.56 * weight) + (1.85 * height) - (4.68 * age)
+
+                    roundHundredthsPlace(
+                        basalMetabolism
+                    ).collect {
+                        emit(it)
+                    }
+                }
+            }
+        }
+
+    private fun roundHundredthsPlace(double: Double): Flow<Double> = flow {
+        emit(
+            (double * 10.0).roundToInt() / 10.0
+        )
     }
 }
--- app/src/test/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismUseCaseTest.kt
+++ app/src/test/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismUseCaseTest.kt
@@ -2,6 +2,10 @@ package com.example.diet.lifestyle.usecase
 
 import com.example.diet.lifestyle.model.Gender
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.fail
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 class CalculateBasalMetabolismUseCaseTest {
@@ -12,39 +16,72 @@ class CalculateBasalMetabolismUseCaseTest {
     fun `남성 사용자가 입력한 체중 신장 나이가 모두 음수가 아닐경우_기초대사랑을 계산함`() {
         val expected = 1979.2
 
-        val result = calculateBasalMetabolismUseCase(84.0, 184.0, 24, Gender.Male)
-
-        assertEquals(expected, result)
+        runBlocking {
+            calculateBasalMetabolismUseCase(
+                84.0,
+                184.0,
+                24,
+                Gender.Male
+            ).catch {
+                fail()
+            }.collect {
+                assertEquals(expected, it)
+            }
+        }
     }
 
     @Test
     fun `여성 사용자가 입력한 체중 신장 나이가 모두 음수가 아닐경우_기초대사랑을 계산함`() {
         val expected = 1686.2
 
-        val result = calculateBasalMetabolismUseCase(84.0, 184.0, 24, Gender.Female)
-
-        assertEquals(expected, result)
+        runBlocking {
+            calculateBasalMetabolismUseCase(
+                84.0,
+                184.0,
+                24,
+                Gender.Female
+            ).catch {
+                fail()
+            }.collect {
+                assertEquals(expected, it)
+            }
+        }
     }
 
     @Test
     fun `남성 사용자가 입력한 체중이 음수인 경우 경우_에러 출력`() {
         val expected = IllegalArgumentException()
 
-        try {
-            calculateBasalMetabolismUseCase(-84.0, 184.0, 24, Gender.Male)
-        } catch (e: Exception) {
-            assertEquals(expected::class, e::class)
+        runBlocking {
+            calculateBasalMetabolismUseCase(
+                -84.0,
+                184.0,
+                24,
+                Gender.Male
+            ).catch { cause ->
+                assertEquals(expected::class, cause::class)
+            }.collect {
+                fail()
+            }
         }
+
     }
 
     @Test
     fun `남성 사용자가 입력한 신장이 음수인 경우 경우_에러 출력`() {
         val expected = IllegalArgumentException()
 
-        try {
-            calculateBasalMetabolismUseCase(84.0, -184.0, 24, Gender.Male)
-        } catch (e: Exception) {
-            assertEquals(expected::class, e::class)
+        runBlocking {
+            calculateBasalMetabolismUseCase(
+                84.0,
+                -184.0,
+                24,
+                Gender.Male
+            ).catch { cause ->
+                assertEquals(expected::class, cause::class)
+            }.collect {
+                fail()
+            }
         }
     }
 
@@ -52,10 +89,17 @@ class CalculateBasalMetabolismUseCaseTest {
     fun `남성 사용자가 입력한 나이가 음수인 경우 경우_에러 출력`() {
         val expected = IllegalArgumentException()
 
-        try {
-            calculateBasalMetabolismUseCase(84.0, 184.0, -24, Gender.Male)
-        } catch (e: Exception) {
-            assertEquals(expected::class, e::class)
+        runBlocking {
+            calculateBasalMetabolismUseCase(
+                84.0,
+                184.0,
+                -24,
+                Gender.Male
+            ).catch { cause ->
+                assertEquals(expected::class, cause::class)
+            }.collect {
+                fail()
+            }
         }
     }
 }
