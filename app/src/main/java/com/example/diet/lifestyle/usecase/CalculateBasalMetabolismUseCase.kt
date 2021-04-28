b8fb39d11b2155739f7ed8f7acf2b1bd4f4ea7c9
Laskel96 <tls09611@naver.com>
2021-04-28 22:32:41 +0900

    Add roundTo<Nth>Digit Method

--- app/src/main/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismUseCase.kt
+++ app/src/main/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismUseCase.kt
@@ -4,6 +4,10 @@ import com.example.diet.lifestyle.model.Gender
 import kotlin.math.roundToInt
 
 class CalculateBasalMetabolismUseCase {
+    private fun roundToFirstDigit(double: Double) : Double{
+        return (double * 10.0).roundToInt() / 10.0
+    }
+
     operator fun invoke(weight: Double, height: Double, age: Int, gender: Gender): Double {
         require(weight > 0 && height > 0 && age > 0) {
             "Weight, Height, Age Must Be non-negative"
@@ -11,12 +15,12 @@ class CalculateBasalMetabolismUseCase {
         return when (gender) {
             Gender.male -> {
                 val basalMetabolism: Double = 66.47 + (13.75 * weight) + (5 * height) - (6.76 * age)
-                (basalMetabolism * 10).roundToInt() / 10.0
+                roundToFirstDigit(basalMetabolism)
             }
             Gender.female -> {
                 val basalMetabolism: Double =
                     655.1 + (9.56 * weight) + (1.85 * height) - (4.68 * age)
-                (basalMetabolism * 10).roundToInt() / 10.0
+                roundToFirstDigit(basalMetabolism)
             }
         }
     }
