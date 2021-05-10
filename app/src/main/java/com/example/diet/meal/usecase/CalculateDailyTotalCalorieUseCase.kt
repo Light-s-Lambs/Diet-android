d259f5d79414af8713264b2c58de7a1f4a432181
K <krbgh205760@naver.com>
2021-05-11 01:32:44 +0900

    일일 식단 칼로리 계산 UseCase 이름 변경(CalculateTotalCalorieUseCase -> CalculateDailyTotalCalorieUseCase)

--- app/src/main/java/com/example/diet/meal/usecase/CalculateDailyTotalCalorieUseCase.kt
+++ app/src/main/java/com/example/diet/meal/usecase/CalculateDailyTotalCalorieUseCase.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import org.joda.time.DateTime
 
-class CalculateTotalCalorieUseCase(
+class CalculateDailyTotalCalorieUseCase(
     private val repository: MealRepository
 ) {
     suspend operator fun invoke(date: DateTime) = flowOf(repository.getDailyMealList(date).first().sumByDouble { meal -> meal.calorie })
