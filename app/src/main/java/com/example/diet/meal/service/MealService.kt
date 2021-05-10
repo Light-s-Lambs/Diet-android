f7e60d4557350b97bb8c70d4520ccea8ca03c8f0
K <krbgh205760@naver.com>
2021-05-11 01:25:02 +0900

    UI Layer를 위한 MealService 인터페이스 추가

---
+++ app/src/main/java/com/example/diet/meal/service/MealService.kt
@@ -0,0 +1,9 @@
+package com.example.diet.meal.service
+
+import com.example.diet.meal.model.Meal
+import kotlinx.coroutines.flow.Flow
+import org.joda.time.DateTime
+
+interface MealService {
+    fun reloadDailyMealList(date: DateTime, totalCalorie: Double, dailyMealList: List<Meal>): Flow<Unit>
+}
