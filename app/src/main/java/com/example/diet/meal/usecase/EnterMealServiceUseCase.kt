418da3d8c0366105b9ae7d7d82580538087cc6a8
K <krbgh205760@naver.com>
2021-05-11 01:34:14 +0900

    식단 화면 진입 UseCase 추가

---
+++ app/src/main/java/com/example/diet/meal/usecase/EnterMealServiceUseCase.kt
@@ -0,0 +1,21 @@
+package com.example.diet.meal.usecase
+
+import com.example.diet.meal.repository.MealRepository
+import com.example.diet.meal.service.MealService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import org.joda.time.DateTime
+
+class EnterMealServiceUseCase(
+    private val mealService: MealService,
+    private val repository: MealRepository
+) {
+    suspend operator fun invoke(date: DateTime): Flow<Unit> {
+        val dailyMealList = repository.getDailyMealList(date).first()
+        val calculateTotalCalorieUseCase = CalculateDailyTotalCalorieUseCase(repository)
+        val totalCalorie = calculateTotalCalorieUseCase(date).first()
+        mealService.reloadDailyMealList(date, totalCalorie, dailyMealList)
+        return flowOf()
+    }
+}
