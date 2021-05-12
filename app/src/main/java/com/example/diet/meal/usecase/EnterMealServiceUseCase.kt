ebc395fc54541aaa6498b17b58ee7947ebf63331
K <krbgh205760@naver.com>
2021-05-12 21:59:53 +0900

    코루틴를 제거하고 flow를 활용하는 코드로 변경

--- app/src/main/java/com/example/diet/meal/usecase/EnterMealServiceUseCase.kt
+++ app/src/main/java/com/example/diet/meal/usecase/EnterMealServiceUseCase.kt
@@ -2,20 +2,15 @@ package com.example.diet.meal.usecase
 
 import com.example.diet.meal.repository.MealRepository
 import com.example.diet.meal.service.MealService
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import org.joda.time.DateTime
 
 class EnterMealServiceUseCase(
     private val mealService: MealService,
     private val repository: MealRepository
 ) {
-    suspend operator fun invoke(date: DateTime): Flow<Unit> {
-        val dailyMealList = repository.getDailyMealList(date).first()
-        val calculateTotalCalorieUseCase = CalculateDailyTotalCalorieUseCase(repository)
-        val totalCalorie = calculateTotalCalorieUseCase(date).first()
-        mealService.refreshDailyMealListUI(date, totalCalorie, dailyMealList)
-        return flowOf()
+    operator fun invoke(date: DateTime) {
+        val calculateDailyTotalCalorieUseCase = CalculateDailyTotalCalorieUseCase()
+        repository.getDailyMealList(date).map { mealService.refreshDailyMealListUI(date, calculateDailyTotalCalorieUseCase(it), it) }
     }
 }
