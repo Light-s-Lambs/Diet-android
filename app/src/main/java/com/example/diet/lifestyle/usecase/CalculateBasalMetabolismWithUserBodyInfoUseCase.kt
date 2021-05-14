025563ae62c542e0b0c82d82dd567fe2a327798c
Laskel96 <tls09611@naver.com>
2021-05-14 23:47:48 +0900

    Remove Unnecessary collect

--- app/src/main/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismWithUserBodyInfoUseCase.kt
+++ app/src/main/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismWithUserBodyInfoUseCase.kt
@@ -3,8 +3,7 @@ package com.example.diet.lifestyle.usecase
 import com.example.diet.extension.timeout
 import com.example.diet.lifestyle.service.UserBodyInfoService
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flatMapConcat
 
 @kotlinx.coroutines.FlowPreview
 class CalculateBasalMetabolismWithUserBodyInfoUseCase(
@@ -12,20 +11,17 @@ class CalculateBasalMetabolismWithUserBodyInfoUseCase(
 ) {
     val calculateBasalMetabolismUseCase = CalculateBasalMetabolismUseCase()
 
-    operator fun invoke(): Flow<Double> = flow {
+    suspend operator fun invoke(): Flow<Double> =
         userBodyInfoService.getCurrentUserBodyInfo()
             .timeout(2000)
-            .collect {
+            .flatMapConcat { userBodyInfo ->
                 calculateBasalMetabolismUseCase(
-                    it.weight,
-                    it.height,
-                    it.age,
-                    it.gender
-                ).collect { basalMetabolism ->
-                    emit(basalMetabolism)
-                }
+                    userBodyInfo.weight,
+                    userBodyInfo.height,
+                    userBodyInfo.age,
+                    userBodyInfo.gender
+                )
             }
-    }
 }
 
 
