a419af9c21a5a45821cd1ea34e9fdda0b71baa66
Laskel96 <tls09611@naver.com>
2021-05-18 01:41:22 +0900

    Add Flow

--- app/src/main/java/com/example/diet/weightInfo/repository/WeightInfoRepository.kt
+++ app/src/main/java/com/example/diet/weightInfo/repository/WeightInfoRepository.kt
@@ -1,11 +1,12 @@
 package com.example.diet.weightInfo.repository
 
 import com.example.diet.weightInfo.model.WeightInfo
+import kotlinx.coroutines.flow.Flow
 import org.joda.time.DateTime
 
 interface WeightInfoRepository {
-    fun createWeightInfo(date: DateTime, weightInfo: WeightInfo): Boolean
-    fun loadWeightInfo(date: DateTime): WeightInfo
-    fun updateWeightInfo(date: DateTime, weightInfo: WeightInfo): Boolean
-    fun deleteWeightInfo(date: DateTime): Boolean
+    fun createWeightInfo(date: DateTime, weightInfo: WeightInfo): Flow<WeightInfo>
+    fun loadWeightInfo(date: DateTime): Flow<WeightInfo>
+    fun updateWeightInfo(date: DateTime, weightInfo: WeightInfo): Flow<WeightInfo>
+    fun deleteWeightInfo(date: DateTime): Flow<WeightInfo>
 }
--- app/src/main/java/com/example/diet/weightInfo/usecase/LoadWeightInfoUseCase.kt
+++ app/src/main/java/com/example/diet/weightInfo/usecase/LoadWeightInfoUseCase.kt
@@ -2,10 +2,11 @@ package com.example.diet.weightInfo.usecase
 
 import com.example.diet.weightInfo.model.WeightInfo
 import com.example.diet.weightInfo.repository.WeightInfoRepository
+import kotlinx.coroutines.flow.Flow
 import org.joda.time.DateTime
 
 class LoadWeightInfoUseCase(
     private val repository: WeightInfoRepository
 ) {
-    operator fun invoke(date: DateTime): WeightInfo = repository.loadWeightInfo(date)
+    operator fun invoke(date: DateTime): Flow<WeightInfo> = repository.loadWeightInfo(date)
 }
