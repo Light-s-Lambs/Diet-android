5ae5bdd1813213a8f11d302f136795e786fc22ac
Laskel96 <tls09611@naver.com>
2021-05-20 00:33:05 +0900

    Return new flow

--- app/src/main/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismWithUserBodyInfoUseCase.kt
+++ app/src/main/java/com/example/diet/lifestyle/usecase/CalculateBasalMetabolismWithUserBodyInfoUseCase.kt
@@ -4,13 +4,14 @@ import com.example.diet.extension.timeout
 import com.example.diet.lifestyle.repository.UserBodyInfoRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flow
 
 @kotlinx.coroutines.FlowPreview
 class CalculateBasalMetabolismWithUserBodyInfoUseCase(
     private val userBodyInfoRepository: UserBodyInfoRepository,
     private val calculateBasalMetabolismUseCase: CalculateBasalMetabolismUseCase
 ) {
-    suspend operator fun invoke(): Flow<Double> =
+    operator fun invoke(): Flow<Double> = flow {
         userBodyInfoRepository.getCurrentUserBodyInfo()
             .timeout(2000)
             .flatMapConcat { userBodyInfo ->
@@ -21,6 +22,7 @@ class CalculateBasalMetabolismWithUserBodyInfoUseCase(
                     userBodyInfo.gender
                 )
             }
+    }
 }
 
 
