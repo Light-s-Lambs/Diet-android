b47e2fe682c94293bf84acd0e4e8d53073dadf4d
K <krbgh205760@naver.com>
2021-06-14 22:58:29 +0900

    CalculateTotalCalorieUseCase의 invoke의 반환값을 Double에서 Flow<Double>로 변경

--- app/src/main/java/com/example/diet/meal/usecase/CalculateDailyTotalCalorieUseCase.kt
+++ app/src/main/java/com/example/diet/meal/usecase/CalculateDailyTotalCalorieUseCase.kt
@@ -1,8 +1,10 @@
 package com.example.diet.meal.usecase
 
 import com.example.diet.meal.model.Meal
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class CalculateDailyTotalCalorieUseCase {
-    operator fun invoke(dailyMealList: List<Meal>) =
-        dailyMealList.sumByDouble { meal -> meal.calorie }
+    operator fun invoke(dailyMealList: Flow<List<Meal>>): Flow<Double> =
+        dailyMealList.map { it.sumByDouble { meal -> meal.calorie } }
 }
